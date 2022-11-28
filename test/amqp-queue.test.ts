4bf34276507372c4a26f90f203b8427b0be61959
Cyril CHAPON <cyril@permettezmoideconstruire.fr>
2022-11-28 05:40:20 +0100

    Remove old tests in queue

--- test/amqp-queue.test.ts
+++ test/amqp-queue.test.ts
@@ -1,37 +1,11 @@
 import { describe, it } from 'mocha'
-// import { assert } from 'chai'
+import { assert } from 'chai'
 
 import AmqpQueue from '../src/amqp-queue'
 import Amqp from '../src/amqp'
 import { Replies, Channel } from 'amqplib'
 
 describe('AmqpQueue', () => {
-  // describe('_checkName', () => {
-  //   it('should pass', () => {
-  //     const queue = new AmqpQueue(amqp, 'my-queue')
-  //     queue._checkName()
-  //   })
-
-  //   it('should throw without name', () => {
-  //     const queue = new AmqpQueue({})
-  //     assert.throws(queue._checkName.bind(queue))
-  //     queue.name = null
-  //     assert.throws(queue._checkName.bind(queue))
-  //     queue.name = undefined
-  //     assert.throws(queue._checkName.bind(queue))
-  //     queue.name = 0
-  //     assert.throws(queue._checkName.bind(queue))
-  //     queue.name = ''
-  //     assert.throws(queue._checkName.bind(queue))
-  //   })
-
-  //   it('shouldn\'t throw with name set afterward', () => {
-  //     const queue = new AmqpQueue({})
-  //     queue.name = 'queue-name'
-  //     assert.doesNotThrow(queue._checkName.bind(queue))
-  //   })
-  // })
-
   describe('assert', () => {
     it('should pass', async () => {
       /* eslint-disable @typescript-eslint/consistent-type-assertions */
@@ -46,31 +20,5 @@ describe('AmqpQueue', () => {
       const queue = new AmqpQueue(amqp, 'my-queue')
       await queue.assert()
     })
-
-    // it('should pass without queue name', async () => {
-    //   const amqp = <Amqp> {
-    //     channel: <Channel><unknown> {
-    //       assertQueue: () => (<Bluebird<Replies.AssertQueue>> Promise.resolve(<Replies.AssertQueue>{
-    //         queue: 'my-queue'
-    //       }))
-    //     }
-    //   }
-
-    //   const queue = new AmqpQueue(amqp)
-    //   await queue.assert()
-    // })
-
-    // it('should fill name', async () => {
-    //   const queue = new AmqpQueue({
-    //     channel: {
-    //       assertQueue: async () => ({
-    //         queue: 'my-queue'
-    //       })
-    //     }
-    //   }, null)
-    //   await queue.assert()
-    //   assert.ok(queue.name)
-    //   assert.strictEqual(queue.name, 'my-queue')
-    // })
   })
 })
